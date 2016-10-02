035e8a9ae0c6591476a1ba6f7090117b04c53cf9
Alberto~Ferioli <albertobebz@gmail.com>
2016-10-02 21:45:23 +0100

    app scaffolding

---
+++ scripts/services/mainService.js
@@ -0,0 +1,15 @@
+app.service("dataService", function($http){
+
+    this.getTodos = function(callback){
+        $http.get("mock/todos.json")
+            .then(callback)
+    };
+
+    this.deleteTodo = function(todo){
+        console.log(todo.name + " has been deleted");
+    };
+
+    this.saveTodo = function(todo){
+        console.log(todo.name + " has been saved");
+    };
+});
