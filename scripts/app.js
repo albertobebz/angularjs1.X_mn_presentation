a869d39fddd88371ace3cf847d271a1c31e2fe60
Alberto~Ferioli <albertobebz@gmail.com>
2016-10-02 22:58:45 +0100

    add more routes

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("todoListApp", []);
+var app = angular.module("todoListApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'mainCtrl',
+      templateUrl: 'views/home.html'
+    })
+    .when('/test1', {
+      templateUrl: "views/test.html"
+    })
+    .otherwise({
+        template : "<h1>Nothing has been selected</h1>"
+    });
+});
