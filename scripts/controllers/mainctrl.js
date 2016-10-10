328f80cc0437b5f0efa045ddc02bb38c2b775b7f
albertobebz <alberto@mumsnet.com>
2016-10-10 19:46:29 +0200

    tidy up the code

--- scripts/controllers/mainctrl.js
+++ scripts/controllers/mainctrl.js
@@ -1,23 +1,26 @@
+'use strict';
+
 app.controller("mainCtrl", function($scope, dataService){
 
-    $scope.addTodo = function(){
-        var todo = {
-            name: "new task",
-            date: new Date()
-        };
-        $scope.todos.push(todo);
-    }
+  $scope.addTodo = function(){
+    var todo = {
+      name: "new task",
+      date: new Date()
+    };
+    $scope.todos.push(todo);
+  }
 
-    dataService.getTodos(function(response){
-        $scope.todos = response.data;
-    });
+  dataService.getTodos(function(response){
+    scope.todos = response.data;
+  });
 
-    $scope.deleteTodo = function(todo, $index){
-        dataService.deleteTodo(todo);
-        $scope.todos.splice($index, 1);
-    }
+  $scope.deleteTodo = function(todo, $index){
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  }
 
-    $scope.saveTodo = function(todo){
-        dataService.saveTodo(todo);
-    }
+  $scope.saveTodo = function(todo){
+    dataService.saveTodo(todo);
+  }
+
 });
